b0e2e5208cb6e2d30bfe166a33604b38b1bec6ed
Open Cipher <8209379+open-cipher@users.noreply.github.com>
2025-10-18 18:37:50 +0530

    docs: update primary colors

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -15,15 +15,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          My App
+         <HexagonIcon className="size-5 text-purple-500" />
+          OpenHive
         </>
       ),
     },
